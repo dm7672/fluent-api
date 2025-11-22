0ae2e82a3637a0af2d45994bc88d5f6553d20aa6
dm7672 <dmitrsem10@gmail.com>
2025-11-22 11:07:47 +0500

    Добавил Serializer

---
+++ ObjectPrinting/Serializer.cs
@@ -0,0 +1,214 @@
+﻿// SerializerStatic.cs
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace ObjectPrinting
+{
+    public static class Serializer
+    {
+        public static string Serialize<T>(T root, PrintingConfig<T> config)
+        {
+            var visited = new HashSet<object>(new ReferenceEqualityComparer());
+            var sb = new StringBuilder();
+            PrintObject(root, 0, null);
+            return sb.ToString();
+
+            void PrintObject(object? obj, int nestingLevel, MemberInfo? currentMember)
+            {
+                if (obj == null) { sb.AppendLine("null"); return; }
+
+                var type = obj.GetType();
+
+                if (IsExcluded(type, currentMember))
+                {
+                    sb.AppendLine(string.Empty);
+                    return;
+                }
+
+                if (TryApplyMemberSerializer(obj, currentMember)) return;
+                if (TryApplyTypeSerializer(type, obj)) return;
+
+                if (HandleString(obj, currentMember)) return;
+                if (HandleFormattable(type, obj)) return;
+                if (HandleFinals(type, obj)) return;
+
+                if (HandleReferenceTracking(type, obj)) return;
+
+                if (HandleDictionary(obj, nestingLevel)) return;
+                if (HandleEnumerable(obj, nestingLevel)) return;
+
+                sb.AppendLine(type.Name);
+                PrintProperties(type, obj, nestingLevel);
+                PrintFields(type, obj, nestingLevel);
+            }
+
+            static string Indent(int lvl) => new string('\t', lvl);
+
+            static object? GetValueSafely(MemberInfo member, object obj)
+            {
+                try
+                {
+                    switch (member)
+                    {
+                        case PropertyInfo p: return p.GetValue(obj);
+                        case FieldInfo f: return f.GetValue(obj);
+                        default: return null;
+                    }
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+
+            bool IsExcluded(Type type, MemberInfo? member) =>
+                config.ExcludedTypes.Contains(type) || (member != null && config.ExcludedMembers.Contains(member));
+
+            bool TryApplyMemberSerializer(object obj, MemberInfo? member)
+            {
+                if (member == null || !config.MemberSerializers.TryGetValue(member, out var mser)) return false;
+                var s = mser.DynamicInvoke(obj);
+                sb.AppendLine(s?.ToString());
+                return true;
+            }
+
+            bool TryApplyTypeSerializer(Type type, object obj)
+            {
+                if (!config.TypeSerializers.TryGetValue(type, out var tser)) return false;
+                var s = tser.DynamicInvoke(obj);
+                sb.AppendLine(s?.ToString());
+                return true;
+            }
+
+            bool HandleString(object? obj, MemberInfo? member)
+            {
+                if (obj == null) return false;
+                if (obj.GetType() != typeof(string)) return false;
+
+                var s = obj as string;
+                if (member != null && config.MemberTrimLengths.TryGetValue(member, out var l) && s != null && s.Length > l)
+                    s = s.Substring(0, l);
+
+                sb.AppendLine(s);
+                return true;
+            }
+
+            bool HandleFormattable(Type type, object obj)
+            {
+                if (obj is not IFormattable formattable || !config.TypeCultures.TryGetValue(type, out var culture))
+                    return false;
+                sb.AppendLine(formattable.ToString(null, culture));
+                return true;
+            }
+
+            bool HandleFinals(Type type, object obj)
+            {
+                if (!config.FinalTypes.Contains(type)) return false;
+                sb.AppendLine(obj.ToString());
+                return true;
+            }
+
+            bool HandleReferenceTracking(Type type, object obj)
+            {
+                if (type.IsValueType) return false;
+                if (visited.Contains(obj))
+                {
+                    sb.AppendLine($"<Циклическая ссылка {type.Name}>");
+                    return true;
+                }
+                visited.Add(obj);
+                return false;
+            }
+
+            bool HandleDictionary(object obj, int nestingLevel)
+            {
+                if (obj is not IDictionary dict) return false;
+
+                var type = obj.GetType();
+                sb.AppendLine(type.Name);
+                foreach (DictionaryEntry e in dict)
+                {
+                    sb.Append(Indent(nestingLevel + 1));
+                    sb.Append("Key = ");
+                    PrintObject(e.Key, nestingLevel + 1, null);
+
+                    sb.Append(Indent(nestingLevel + 1));
+                    sb.Append("Value = ");
+                    PrintObject(e.Value, nestingLevel + 1, null);
+                }
+                return true;
+            }
+
+            bool HandleEnumerable(object obj, int nestingLevel)
+            {
+                if (obj is not IEnumerable enumerable || obj is string) return false;
+
+                var type = obj.GetType();
+                sb.AppendLine(type.Name);
+                int i = 0;
+                foreach (var item in enumerable)
+                {
+                    sb.Append(Indent(nestingLevel + 1));
+                    sb.Append($"[{i}] = ");
+                    PrintObject(item, nestingLevel + 1, null);
+                    i++;
+                }
+                return true;
+            }
+
+            void PrintProperties(Type type, object obj, int nestingLevel)
+            {
+                var props = type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+                foreach (var p in props)
+                {
+                    if (p.GetIndexParameters().Length > 0) continue;
+                    if (config.ExcludedTypes.Contains(p.PropertyType) || config.ExcludedMembers.Contains(p)) continue;
+
+                    sb.Append(Indent(nestingLevel + 1));
+                    sb.Append(p.Name);
+                    sb.Append(" = ");
+
+                    var value = GetValueSafely(p, obj);
+
+                    if (value is string sVal && config.MemberTrimLengths.TryGetValue(p, out var trim) && sVal.Length > trim)
+                        value = sVal.Substring(0, trim);
+
+                    PrintObject(value, nestingLevel + 1, p);
+                }
+            }
+
+            void PrintFields(Type type, object obj, int nestingLevel)
+            {
+                var fields = type.GetFields(BindingFlags.Public | BindingFlags.Instance);
+                foreach (var f in fields)
+                {
+                    if (config.ExcludedTypes.Contains(f.FieldType) || config.ExcludedMembers.Contains(f)) continue;
+
+                    sb.Append(Indent(nestingLevel + 1));
+                    sb.Append(f.Name);
+                    sb.Append(" = ");
+
+                    var value = GetValueSafely(f, obj);
+
+                    if (value is string sf && config.MemberTrimLengths.TryGetValue(f, out var trimf) && sf.Length > trimf)
+                        value = sf.Substring(0, trimf);
+
+                    PrintObject(value, nestingLevel + 1, f);
+                }
+            }
+
+            
+
+        }
+
+        private class ReferenceEqualityComparer : IEqualityComparer<object>
+        {
+            public new bool Equals(object x, object y) => ReferenceEquals(x, y);
+            public int GetHashCode(object obj) => System.Runtime.CompilerServices.RuntimeHelpers.GetHashCode(obj);
+        }
+    }
+}
