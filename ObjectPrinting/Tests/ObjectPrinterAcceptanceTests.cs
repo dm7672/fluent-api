59cd08471c56f6da4c5879b683cb3eb0c3e97900
dm7672 <dmitrsem10@gmail.com>
2025-11-22 11:08:13 +0500

    Добавил тесты

--- ObjectPrinting/Tests/ObjectPrinterAcceptanceTests.cs
+++ ObjectPrinting/Tests/ObjectPrinterAcceptanceTests.cs
@@ -1,27 +1,49 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Globalization;
+using FluentAssertions;
+using NUnit.Framework;
+using ObjectPrinting;
 
 namespace ObjectPrinting.Tests
 {
-    [TestFixture]
     public class ObjectPrinterAcceptanceTests
     {
+
         [Test]
-        public void Demo()
+        public void AcceptanceTest_FluentAssertions()
         {
-            var person = new Person { Name = "Alex", Age = 19 };
+            var person = new Person
+            {
+                Id = Guid.NewGuid(),
+                Name = "Alexander",
+                Age = 19,
+                Height = 1.85
+            };
 
-            var printer = ObjectPrinter.For<Person>();
-                //1. Исключить из сериализации свойства определенного типа
-                //2. Указать альтернативный способ сериализации для определенного типа
-                //3. Для числовых типов указать культуру
-                //4. Настроить сериализацию конкретного свойства
-                //5. Настроить обрезание строковых свойств (метод должен быть виден только для строковых свойств)
-                //6. Исключить из сериализации конкретного свойства
-            
-            string s1 = printer.PrintToString(person);
+            var printerNoGuid = ObjectPrinter.For<Person>()
+                .Excluding<Guid>() 
+                .Printing<double>().Using(CultureInfo.InvariantCulture)
+                .Printing(p => p.Name).TrimmedToLength(5);
 
-            //7. Синтаксический сахар в виде метода расширения, сериализующего по-умолчанию        
-            //8. ...с конфигурированием
+            string resultNoGuid = printerNoGuid.PrintToString(person);
+
+            resultNoGuid.Should().NotBeNullOrWhiteSpace();
+            resultNoGuid.Should().NotContain(person.Id.ToString());
+            resultNoGuid.Should().Contain(person.Name.Substring(0, 5));
+            resultNoGuid.Should().NotContain(person.Name.Substring(0, 6));
+            if (person.Name.Length > 10)
+                resultNoGuid.Should().NotContain(person.Name);
+
+            var printerFormat = ObjectPrinter.For<Person>()
+                .Printing<int>().Using(i => i.ToString("X"));
+
+            string resultFormat = printerFormat.PrintToString(person);
+
+            resultFormat.Should().Contain(person.Age.ToString("X"));
+
+            var printerExcludeAge = ObjectPrinter.For<Person>().Excluding(p => p.Age);
+            string resultExcludeAge = printerExcludeAge.PrintToString(person);
+            resultExcludeAge.Should().NotContain(person.Age.ToString());
         }
     }
 }
---
+++ ObjectPrinting/Tests/PrintToStringTests.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using FluentAssertions;
+using NUnit.Framework;
+using ObjectPrinting;
+
+namespace ObjectPrinting.Tests
+{
+    [TestFixture]
+    public class PrintToStringTests
+    {
+
+        private class Node
+        {
+            public string Name { get; set; }
+            public Node Other { get; set; }
+        }
+
+        private class Container
+        {
+            public int[] Numbers { get; set; }
+            public List<string> List { get; set; }
+            public Dictionary<string, int> Map { get; set; }
+        }
+
+        [Test]
+        public void PrintToString_NullObject_PrintsNull()
+        {
+            var printer = ObjectPrinter.For<Person>();
+            string result = printer.PrintToString(null);
+
+            result.Should().Be("null" + Environment.NewLine);
+        }
+
+        [Test]
+        public void PrintToString_SimpleObject_ContainsTypeAndMembers()
+        {
+            var person = new Person { Id = Guid.NewGuid(), Name = "Петя", Height = 1.82, Age = 19 };
+            string s = ObjectPrinter.For<Person>().PrintToString(person);
+
+            s.Should().Contain("Person");
+            s.Should().Contain("Name");
+            s.Should().Contain("Age");
+            s.Should().Contain("Height");
+            s.Should().Contain("Петя");
+            s.Should().Contain("19");
+        }
+
+        [Test]
+        public void PrintToString_ExcludingType_DoesNotContainValueOfThatType()
+        {
+            var person = new Person { Id = Guid.NewGuid(), Name = "Петя", Height = 1.75, Age = 30 };
+            var printer = ObjectPrinter.For<Person>().Excluding<Guid>();
+
+            string s = printer.PrintToString(person);
+
+            s.Should().NotContain(person.Id.ToString());
+        }
+
+        [Test]
+        public void PrintToString_TypeSerializer_AppliesSerializerForType()
+        {
+            var person = new Person { Name = "X", Age = 255 };
+            var printer = ObjectPrinter.For<Person>()
+                .Printing<int>().Using(i => i.ToString("X")); 
+
+            string s = printer.PrintToString(person);
+
+            s.Should().Contain(person.Age.ToString("X"));
+        }
+
+        [Test]
+        public void PrintToString_TypeCulture_AppliesCultureForIFormattable()
+        {
+            var person = new Person { Name = "Y", Height = 1234.56 };
+            var printer = ObjectPrinter.For<Person>()
+                .Printing<double>().Using(CultureInfo.InvariantCulture);
+
+            string s = printer.PrintToString(person);
+
+            var expected = person.Height.ToString(CultureInfo.InvariantCulture);
+            s.Should().Contain(expected);
+        }
+
+        [Test]
+        public void PrintToString_MemberSerializer_AppliesToSpecificMember()
+        {
+            var person = new Person { Name = "Петя", Age = 20 };
+            var printer = ObjectPrinter.For<Person>()
+                .Printing(p => p.Name).Using(n => $"<{n}>");
+
+            string s = printer.PrintToString(person);
+
+            s.Should().Contain("<Петя>");
+        }
+
+        [Test]
+        public void PrintToString_TrimmedToLength_TruncatesStringMember()
+        {
+            var person = new Person { Name = "Василий", Age = 40 };
+            var printer = ObjectPrinter.For<Person>()
+                .Printing(p => p.Name).TrimmedToLength(3);
+
+            string s = printer.PrintToString(person);
+
+            s.Should().Contain("Вас");
+            s.Should().NotContain("Васи");
+        }
+
+        [Test]
+        public void PrintToString_ExcludingMember_DoesNotContainMemberValue()
+        {
+            var person = new Person { Name = "Петя", Age = 77 };
+            var printer = ObjectPrinter.For<Person>()
+                .Excluding(p => p.Age);
+
+            string s = printer.PrintToString(person);
+
+            s.Should().NotContain("77");
+        }
+
+        [Test]
+        public void PrintToString_CircularReferences_DoesNotStackOverflowAndShowsMarker()
+        {
+            var a = new Node { Name = "A" };
+            var b = new Node { Name = "B" };
+            a.Other = b;
+            b.Other = a;
+
+            string s = ObjectPrinter.For<Node>().PrintToString(a);
+
+            s.Should().Contain("A");
+            s.Should().Contain("B");
+            s.Should().MatchRegex("(?i).*Циклическая.*");
+        }
+
+        [Test]
+        public void PrintToString_Collections_ArraysListsAndDictionariesAreSerialized()
+        {
+            var container = new Container
+            {
+                Numbers = new[] { 1, 2 },
+                List = new List<string> { "x", "y" },
+                Map = new Dictionary<string, int> { { "k", 42 } }
+            };
+
+            string s = ObjectPrinter.For<Container>().PrintToString(container);
+
+            s.Should().Contain("Numbers");
+            s.Should().Contain("[0]");
+            s.Should().Contain("1");
+            s.Should().Contain("List");
+            s.Should().Contain("x");
+            s.Should().Contain("Map");
+            s.Should().Contain("Key");
+            s.Should().Contain("Value");
+            s.Should().Contain("k");
+            s.Should().Contain("42");
+        }
+    }
+}
